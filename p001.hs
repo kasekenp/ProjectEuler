be232ee74788df2c4ce554877b20cf1a098d5bb3
kaseken <kento.kaseda@playground.live>
2020-12-05 17:46:58 +0900

    fix: update p001.hs

--- p001.hs
+++ p001.hs
@@ -1,7 +1,2 @@
-f :: Int -> Int
-f (x) = if x <= 1000
-    then f (x + 1) + (if mod x 3 == 0 || mod x 5 == 0 then x else 0)
-    else 0
-
 main :: IO ()
-main = putStrLn $ show (f 1)
+main = putStrLn $ show $ sum [ x | x <- [1..1000], (x `mod` 3 == 0) || (x `mod` 5 == 0)]
